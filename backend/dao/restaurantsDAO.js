002fe268862bb772e6eb8bfcb450e985317d245e
jbarribal <barribal.joshuageorge@ue.edu.ph>
2022-09-13 20:37:33 +0800

    dao

--- backend/dao/restaurantsDAO.js
+++ backend/dao/restaurantsDAO.js
@@ -29,10 +29,29 @@ export default class RestaurantsDAO {
                 query = {"address.zipcode": {$eq: filters["zipcode"]}}
             }
         }
-    }
+        let cursor
 
-    let cursor
+        try {
+            cursor = await restaurants
+            .find(query)
+        } catch (e) {
+            console.error(`Unable to issue find command, ${e}`)
+            return {restaurantsList: [], totalNumRestaurants: 0}
+            
+        }
 
-    try {
-        
+        const displayCursor = cursor.limit(restaurantsPerPage).skip(restaurantsPerPage * page)
+
+        try {
+            const restaurantsList = await displayCursor.toArray()
+            const totalNumRestaurants = await restaurants.countDocuments(query)
+
+            return {restaurantsList, totalNumRestaurants}
+        } catch (e) {
+            console.error(
+                `Unable to convert cursor to array or problem counting documents, ${e}`,
+            )
+            return {restaurantsList: [], totalNumRestaurants: 0}
+        }
     }
+}
